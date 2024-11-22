8e79313e5a0235f3ec7626953f9e7ae5f0996897
ronaldiputra <ronaldiputra92@gmail.com>
2024-11-23 02:53:22 +0700

    pembaruan index

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
+    // "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "dist/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
